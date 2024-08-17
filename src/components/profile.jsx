c5b8757b3e9da28d8cafc9598de342687c171abf
Alan Joseph <alanpavanathara@hotmail.com>
2024-08-17 21:54:06 +0530

    login complete

--- src/components/profile.jsx
+++ src/components/profile.jsx
@@ -4,37 +4,50 @@ import axios from 'axios';
 import { useUser } from '../context/UserContext';
 
 const Profile = () => {
-    const [user, setUser] = useState(null);
-    const navigate = useNavigate();
+    // const [user, setUser] = useState(null);
+    // const navigate = useNavigate();
     const { username, logout } = useUser();
 
-    useEffect(() => {
-        const fetchProfile = async () => {
-            try {
-                const response = await axios.get('https://sn-backend-gq6u.onrender.com/profile', {
-                    params: { username }
-                });
-                if (response.data.success) {
-                    setUser(response.data.usr);
-                } else {
-                    alert('Failed to fetch profile');
-                }
-            } catch (error) {
-                console.error('Error fetching profile:', error);
-            }
-        };
-
-        fetchProfile();
-    }, [username]);
-
-    const handleLogout = () => {
-        logout(); 
-        navigate('/login'); // Redirect to login page
-    };
-
-    if (!user) {
-        return <p>Loading...</p>;
+    const {user} = useUser();
+    const navigate = useNavigate();
+    if(!user)
+    {
+      navigate('/login');
+      return null;
     }
+    function handleLogout()
+    {
+        logout();
+    }
+  
+
+    // useEffect(() => {
+    //     const fetchProfile = async () => {
+    //         try {
+    //             const response = await axios.get('https://sn-backend-gq6u.onrender.com/profile', {
+    //                 params: { username }
+    //             });
+    //             if (response.data.success) {
+    //                 setUser(response.data.usr);
+    //             } else {
+    //                 alert('Failed to fetch profile');
+    //             }
+    //         } catch (error) {
+    //             console.error('Error fetching profile:', error);
+    //         }
+    //     };
+
+    //     fetchProfile();
+    // }, [username]);
+
+    // const handleLogout = () => {
+    //     logout(); 
+    //     navigate('/login'); // Redirect to login page
+    // };
+
+    // if (!user) {
+    //     return <p>Loading...</p>;
+    // }
 
     return (
         <div style={{ padding: '20px', textAlign: 'center',marginTop:'100px',height:'1000px' }}>
